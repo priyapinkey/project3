e3e49b339140517b18c263ce99f3844d93314ddb
priyapinkey <priyapinkey7588@gmail.com>
2024-09-12 15:24:18 +0530

    new code added

---
+++ src/com/git/Sample.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Sample {
+   public void java() {
+	   System.out.println("java course");
+   }
+   private void selenium() {
+	System.out.println("selenium course");
+}
+   private void sql() {
+	System.out.println("sql course");
+}
+}
